50f488dcd9dd34e53d5deb5c298cc84b2eafb8c4
Danyil Bohdan <danyil.bohdan@gmail.com>
2013-11-03 14:29:22 +0200

    Refactored main operation into a function.
    Also added a check to see if $output is empty before cleaning it
    to avoid error messages.

--- process.sh
+++ process.sh
@@ -2,16 +2,48 @@
 markdown="perl scripts/Markdown_1.0.1/Markdown.pl"
 header="templates/header.html"
 footer="templates/footer.html"
-pages="content/pages"
-static="content/static"
-output="output"
+content="content"
+pages="$content/pages"
+static="$content/static"
+output="$content/output"
 
-if [ "$1" == "init" ]; then
+markdown_to_html() {
+    output_file=$output/$(basename $1 | sed -e "s/.md$//").html
+    echo processing $1 into $output_file
+    cat $header > $output_file
+    $markdown $1 | sed -e "s/^/            /" >> $output_file
+    cat $footer >> $output_file
+}
+
+init() {
     echo Creating directories...
+    mkdir -p $content
     mkdir -p $pages
     mkdir -p $static
     mkdir -p $output
+    cd $content
+    if [ ! -d .git ]; then
+        echo output/\* > .gitignore
+        git init
+    fi
     exit 0
+}
+
+compile_website() {
+    # If $output is not empty...
+    if [ "$(ls -A $output)" ]; then
+        rm -vr $output/*
+    fi
+
+    for file in $pages/*.md; do
+        markdown_to_html $file
+    done
+
+    cp -vR $static/* $output/
+}
+
+if [ "$1" == "init" ]; then
+    init
 fi
 
 
@@ -20,18 +52,4 @@ if [ ! -d $pages -o ! -d $static -o ! -d $output ]; then
     exit 1
 fi
 
-rm -r $output/*
-
-markdown_to_html() {
-    output_file=$output/$(basename $1 | sed -e "s/.md$//").html
-    echo Processing $1 into $output_file
-    cat $header > $output_file
-    $markdown $1 | sed -e "s/^/            /" >> $output_file
-    cat $footer >> $output_file
-}
-
-for file in $pages/*.md; do
-    markdown_to_html $file
-done
-
-cp -vR content/static/* output/
+compile_website
