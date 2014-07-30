8009796a30e61a619a5dec5d81d1943d2ffb1295
a.d.prokofiev@gmail.com <a.d.prokofiev@gmail.com>
2014-07-31 00:19:34 +0200

    Added support for disqus.
    Change variable disqusShortName in website.conf
    Define variable showUserComments on the page where you would like to show comments.
    
    Do not forget to add  <% show-disqus %> function call to your template

--- skeleton/templates/default-procs.tcl
+++ skeleton/templates/default-procs.tcl
@@ -226,3 +226,28 @@ proc format-footer {} {
     }
     return $footer
 }
+
+proc show-disqus {} {
+    global disqusShortName
+    set str ""
+    if {[page-variable-default-get 0 showUserComments]} {
+        set str {
+             <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                }
+        append str "var disqus_shortname = '$disqusShortName'; // required: replace example with your forum shortname \n"
+        append str {
+                /* * * DON'T EDIT BELOW THIS LINE * * */
+                (function() {
+                    var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                    dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+                <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+        }
+    }
+    return $str
+}
