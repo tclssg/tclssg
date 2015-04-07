e597559ccd360e9dc8912a9d1c94aef4cd695312
Danyil Bohdan <danyil.bohdan@gmail.com>
2015-04-07 12:19:21 +0300

    footnotes.tcl: footnote labels.
    This adds support for multiple references to one footnote (with
    just one backlink for now, however).

--- skeleton/templates/footnotes.tcl
+++ skeleton/templates/footnotes.tcl
@@ -6,7 +6,17 @@
 set ::footnotes {}
 set ::footnoteSuffix {}
 
-proc footnote-id {n {link 0}} {
+# Return the footnote number that corresponds to the label $label.
+proc footnote-number {label} {
+    return [expr {
+        [lsearch $::footnotes $label] / 2 + 1
+    }]
+}
+
+# Generate a website-unique footnote id or footnote link id.
+proc footnote-id {label {link 0}} {
+    set n [footnote-number $label]
+
     # Generate a unique footnote suffix that cannot be mistaken for a footnote
     # number by applying a linear bijective map from 0..65535 to 0..65535 to
     # the page id and then formatting the result as four hex digits.
@@ -24,21 +34,47 @@ proc footnote-id {n {link 0}} {
     }
 }
 
-proc footnote {markdown} {
-    lappend ::footnotes [markdown-to-html $markdown]
-    set i [llength $::footnotes]
+# Generate a link to an existing footnote by its label.
+proc footnote-link label {
     return "<span class=\"footnote-link\" id=\"[footnote-id \
-            $i 1]\"><a href=\"#[footnote-id \
-            $i 0]\"><sup>\[$i\]</sup></a></span>"
+        $label 1]\"><a href=\"#[footnote-id \
+        $label 0]\"><sup>\[[footnote-number $label]\]</sup></a></span>"
 }
 
+# Add a new footnote.
+proc footnote args {
+    switch -exact -- [llength $args] {
+        1 {
+            lassign $args markdown
+            set label [expr {
+                [llength [dict keys $::footnotes]] + 1
+            }]
+        }
+        2 {
+            lassign $args label markdown
+            # Avoid conflicts with automatically assigned labels.
+            if {[string is integer -strict $label]} {
+                error "custom footnote labels can not be integer numbers"
+            }
+        }
+        default {
+            error {wrong # args: should be "footnote ?label? markdown"}
+        }
+    }
+    if {[dict exists $::footnotes $label]} {
+        error "footnote \"$label\" already exists"
+    }
+    dict set ::footnotes $label [markdown-to-html $markdown]
+    return [footnote-link $label]
+}
+
+# Generate a list of all footnotes. This is typically used at the end of a page.
 proc footnotes {} {
     set result {<ol class="footnotes">}
-    set i 1
-    foreach footnote $::footnotes {
+    dict for {label footnote} $::footnotes {
         append result "<li class=\"footnote\" id=\"[footnote-id \
-                $i 0]\">$footnote <a href=\"#[footnote-id $i 1]\">↩</a></li>"
-        incr i
+                $label 0]\">$footnote <a href=\"#[footnote-id \
+                $label 1]\">↩</a></li>"
     }
     append result {</ol>}
 }
