3d5a8f555f4750095bd7100ae2d24c9771a5fed3
dbohdan <dbohdan@dbohdan.com>
2018-03-01 18:19:54 +0200

    docs(*): Update copyright

--- tests.tcl
+++ tests.tcl
@@ -1,6 +1,6 @@
 #!/usr/bin/env tclsh
 # Tclssg, a static website generator.
-# Copyright (C) 2013, 2014, 2015, 2016, 2017 dbohdan.
+# Copyright (C) 2013, 2014, 2015, 2016, 2017, 2018 dbohdan.
 # This code is released under the terms of the MIT license. See the file
 # LICENSE for details.
 
