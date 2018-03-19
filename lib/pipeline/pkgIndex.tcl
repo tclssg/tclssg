b0fb8e9d88d6d8a3691d503dc4543d1cb670f864
dbohdan <dbohdan@dbohdan.com>
2018-03-19 18:16:14 +0200

    fix(pipeline): Do not join path in pkgIndex.tcl

--- lib/pipeline/pkgIndex.tcl
+++ lib/pipeline/pkgIndex.tcl
@@ -5,7 +5,7 @@
 
 package ifneeded tclssg::pipeline 0 [list apply {dir {
     foreach path [glob -dir $dir {[0-9]*.tcl}] {
-        uplevel 1 [list source -encoding utf-8 [file join $dir {*}$path]]
+        uplevel 1 [list source -encoding utf-8 $path]
     }
     package provide tclssg::pipeline 0
 }} $dir]
