03c19cb217845c5da2bece3ae700c91e7dccc337
dbohdan <dbohdan@dbohdan.com>
2018-03-29 22:13:19 +0300

    style(browse): Remove tabs mixed in with spaces

--- vendor/browse/browse.tcl
+++ vendor/browse/browse.tcl
@@ -30,11 +30,11 @@ proc ::browse::url {url} {
     global tcl_platform
 
     if {$tcl_platform(platform) eq "windows"} {
-	WinOpenUrl $url
+        WinOpenUrl $url
     } elseif {$tcl_platform(os) eq "Darwin"} {
-	MacOpenUrl $url
+        MacOpenUrl $url
     } else {
-	UnixOpenUrl $url
+        UnixOpenUrl $url
     }
     return
 }
@@ -50,9 +50,9 @@ proc ::browse::WinOpenUrl {url} {
 
     debug.browse {exec $::env(COMSPEC) /c start ($url)}
     if {[catch {
-	exec >NUL: <NUL: $::env(COMSPEC) /c start $url &
+        exec >NUL: <NUL: $::env(COMSPEC) /c start $url &
     } msg]} {
-	Fail $url $msg
+        Fail $url $msg
     }
     return
 }
@@ -65,11 +65,11 @@ proc ::browse::MacOpenUrl {url} {
 
     debug.browse {exec open ($url)}
     if {[catch {
-	#package require Tclapplescript
-	#AppleScript execute "do shell script \"open $url\""
-	exec open $url &
+        #package require Tclapplescript
+        #AppleScript execute "do shell script \"open $url\""
+        exec open $url &
     } msg]} {
-	Fail $url $msg
+        Fail $url $msg
     }
 }
 
@@ -78,28 +78,28 @@ proc ::browse::UnixOpenUrl {url} {
     set redir ">&/dev/null </dev/null"
 
     if {[info exists ::env(BROWSER)]} {
-	set browser $::env(BROWSER)
+        set browser $::env(BROWSER)
     }
 
     if {[info exists ::env(DISPLAY)]} {
-	expr {
-	      [info exists browser]              ||
-	      [FindExecutable xdg-open  browser] ||
-	      [FindExecutable chrome    browser] ||
-	      [FindExecutable firefox   browser] ||
-	      [FindExecutable mozilla   browser] ||
-	      [FindExecutable netscape  browser] ||
-	      [FindExecutable iexplorer browser] ||
-	      [FindExecutable opera     browser] ||
-	      [FindExecutable lynx      browser]
-	  }
+        expr {
+              [info exists browser]              ||
+              [FindExecutable xdg-open  browser] ||
+              [FindExecutable chrome    browser] ||
+              [FindExecutable firefox   browser] ||
+              [FindExecutable mozilla   browser] ||
+              [FindExecutable netscape  browser] ||
+              [FindExecutable iexplorer browser] ||
+              [FindExecutable opera     browser] ||
+              [FindExecutable lynx      browser]
+          }
     } else {
-	# Terminal only.
-	expr {
-	      [info exists browser]              ||
-	      [FindExecutable xdg-open  browser] ||
-	      [FindExecutable lynx      browser]
-	  }
+        # Terminal only.
+        expr {
+              [info exists browser]              ||
+              [FindExecutable xdg-open  browser] ||
+              [FindExecutable lynx      browser]
+          }
     }
 
     # lynx can also output formatted text to a variable
@@ -107,28 +107,28 @@ proc ::browse::UnixOpenUrl {url} {
     # set formatted_text [ exec lynx -dump $url ] - PSE
 
     if {![info exists browser]} {
-	Fail $url "Could not find a browser to use"
-	#return -code error "Could not find a browser to use"
+        Fail $url "Could not find a browser to use"
+        #return -code error "Could not find a browser to use"
     }
 
     if {[string equal [file tail $browser] netscape]} {
-	# -remote url is not understood, only
-	# -remote openUrl(url)
-	if {
-	    [catch {RunBrowser $browser -remote openUrl($url) } msg] &&
-	    [catch {RunBrowser $browser                 $url &} msg]
-	} {
-	    Fail $url "Browser \"$browser\": $msg"
-	}
+        # -remote url is not understood, only
+        # -remote openUrl(url)
+        if {
+            [catch {RunBrowser $browser -remote openUrl($url) } msg] &&
+            [catch {RunBrowser $browser                 $url &} msg]
+        } {
+            Fail $url "Browser \"$browser\": $msg"
+        }
     } else {
-	# Assume that browser may understand -remote url
-	# perhaps browser doesn't understand -remote flag
-	if {
-	    [catch {RunBrowser $browser -remote $url  } msg] &&
-	    [catch {RunBrowser $browser         $url &} msg]
-	} {
-	    Fail $url "Browser \"$browser\": $msg"
-	}
+        # Assume that browser may understand -remote url
+        # perhaps browser doesn't understand -remote flag
+        if {
+            [catch {RunBrowser $browser -remote $url  } msg] &&
+            [catch {RunBrowser $browser         $url &} msg]
+        } {
+            Fail $url "Browser \"$browser\": $msg"
+        }
     }
     return
 }
@@ -138,13 +138,13 @@ proc ::browse::Fail {url msg} {
 
     set msg "Error displaying url \"$url\":\n$msg"
     if {[catch {package present Tk}]} {
-	return -code error -errorcode {BROWSE FAIL} $msg
+        return -code error -errorcode {BROWSE FAIL} $msg
     } else {
-	tk_messageBox \
-	    -title "Error displaying URL" \
-	    -icon error \
-	    -type ok \
-	    -message $msg
+        tk_messageBox \
+            -title "Error displaying URL" \
+            -icon error \
+            -type ok \
+            -message $msg
     }
     return
 }
@@ -155,7 +155,7 @@ proc ::browse::FindExecutable {progname varname} {
     upvar 1 $varname result
     set progs [auto_execok $progname]
     if {[llength $progs]} {
-	set result [lindex $progs 0]
+        set result [lindex $progs 0]
     }
     return [llength $progs]
 }
