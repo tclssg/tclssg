41669a4cb823b245b6d4b9b55c5f2e11d805bb27
dbohdan <dbohdan@dbohdan.com>
2019-03-11 12:46:28 +0200

    feat(1-load-presets): allow comments in presets

--- lib/pipeline/1-load-presets.tcl
+++ lib/pipeline/1-load-presets.tcl
@@ -1,5 +1,5 @@
 # Tclssg, a static website generator.
-# Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018
+# Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019
 # dbohdan and contributors listed in AUTHORS. This code is released under
 # the terms of the MIT license. See the file LICENSE for details.
 
@@ -14,7 +14,11 @@ namespace eval ::tclssg::pipeline::1-load-presets {
                 set id [utils::replace-path-root $file $inputDir {}]
                 if {[regexp {^/?presets} $id]} {
                     log::info "reading preset file [list $file]"
-                    set settings [utils::read-file -translation binary $file]
+
+                    set bin [utils::read-file -translation binary $file]
+
+                    set settings [utils::remove-comments $bin]
+
                     if {[dict exists $settings presets]} {
                         error [list forbidden setting {"presets"} in preset \
                                     file $file]
