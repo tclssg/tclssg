33b6e7eedaf8a16ad084229768b1a033b4615b8f
dbohdan <dbohdan@dbohdan.com>
2019-03-12 16:11:06 +0200

    refactor(skeleton): rename demo plug-in

--- skeleton/plugins/50-plugin-demo.tcl
+++ skeleton/plugins/50-plugin-demo.tcl
@@ -1,9 +1,9 @@
 # Tclssg, a static website generator.
-# Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018
+# Copyright (c) 2013, 2014, 2015, 2016, 2017, 2018, 2019
 # dbohdan and contributors listed in AUTHORS. This code is released under
 # the terms of the MIT license. See the file LICENSE for details.
 
-namespace eval ::tclssg::pipeline::5-plugin-demo {
+namespace eval ::tclssg::pipeline::50-plugin-demo {
     namespace path ::tclssg
     lappend ::tclssg::config::schema greeting
 
