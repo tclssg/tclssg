18cc928fe40232dd9cc210804f0448e4feacb858
dbohdan <dbohdan@dbohdan.com>
2019-07-27 09:46:35 +0000

    fix(90-write-everything): jail output paths

--- lib/pipeline/90-write-everything.tcl
+++ lib/pipeline/90-write-everything.tcl
@@ -7,14 +7,21 @@ namespace eval ::tclssg::pipeline::90-write-everything {
 
     proc transform {} {
         set outputDir [db config get outputDir]
+        set outputDirNorm [fileutil::fullnormalize $outputDir]
+
+        # Restore the original $outputDir prefix for logging.
+        set shorten [list apply {{fromDir toDir path} {
+            list [::tclssg::utils::replace-path-root $path $fromDir $toDir]
+        }} $outputDirNorm $outputDir]
+
         db eval {SELECT file, content FROM output} row {
-            set dest [file join $outputDir $row(file)]
+            set dest [fileutil::jail $outputDir $row(file)]
             set subdir [file dirname $dest]
             if {![file isdir $subdir]} {
-                log::info "creating directory [list $subdir]"
+                log::info "creating directory [{*}$shorten $subdir]"
                 file mkdir $subdir
             }
-            log::info "writing output file [list $dest]"
+            log::info "writing output file [{*}$shorten $dest]"
             utils::write-file -translation binary $dest $row(content)
         }
     }
