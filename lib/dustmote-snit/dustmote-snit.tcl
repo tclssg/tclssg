f512e74cb161b3f8c837523569c091f419f8dfe5
D. Bohdan <dbohdan@dbohdan.com>
2024-05-09 16:11:25 +0000

    docs(dustmote-snit): add MIT-0 license
    It is better to use a standard public-domain-equivalent license
    than a custom public domain dedication.

--- lib/dustmote-snit/dustmote-snit.tcl
+++ lib/dustmote-snit/dustmote-snit.tcl
@@ -1,7 +1,27 @@
-#!/usr/bin/env tclsh
-# DustMote HTTP server originally developed by Harold Kaplan
-# (https://wiki.tcl-lang.org/4333). Modified by D. Bohdan.
-# This code is in the public domain.
+#! /usr/bin/env tclsh
+# DustMote-Snit, an expanded fork of the DustMote HTTP server.
+# DustMote was originally developed and released into the public domain
+# by Harold Kaplan.
+# See <https://wiki.tcl-lang.org/4333>.
+#
+# License:
+#
+# MIT No Attribution
+#
+# Copyright 2015-2018, 2020, 2023-2024 D. Bohdan
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this
+# software and associated documentation files (the "Software"), to deal in the Software
+# without restriction, including without limitation the rights to use, copy, modify,
+# merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+# PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 package require Tcl       8.5 9
 package require fileutil  1
