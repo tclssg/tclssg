1dfe8c6077a2f2cc76ebe364f3b0e7685727a1ca
D. Bohdan <dbohdan@dbohdan.com>
2024-10-01 09:44:17 +0000

    ci(github): install Tcllib 2.0 on BSD and Linux

--- ci/install-deps.sh
+++ ci/install-deps.sh
@@ -1,20 +1,51 @@
 #! /bin/sh
 set -e
 
+tcllib_dir=tcllib-tcllib-2-0
+tcllib_url=https://github.com/tcltk/tcllib/archive/refs/tags/tcllib-2-0.tar.gz
+
 if [ "$(uname)" = Linux ]; then
-    apt-get install -y cmark libsqlite3-tcl tcl tcllib
+    apt-get install -y cmark libsqlite3-tcl make tcl wget
+
+    wget --output-document tcllib.tar.gz --quiet "$tcllib_url"
+    tar xzf tcllib.tar.gz
+
+    cd "$tcllib_dir"
+    ./configure
+    make install
 fi
 
 if [ "$(uname)" = FreeBSD ]; then
-    pkg install -y cmark tcl-sqlite3 tcl86 tcllib
+    pkg install -y cmark tcl-sqlite3 tcl86
     ln -s /usr/local/bin/tclsh8.6 /usr/local/bin/tclsh
+
+    fetch -o tcllib.tar.gz "$tcllib_url"
+    tar xzf tcllib.tar.gz
+
+    cd "$tcllib_dir"
+    ./configure
+    make install
 fi
 
 if [ "$(uname)" = NetBSD ]; then
-    pkgin -y install cmark tcl tcllib
+    pkgin -y install cmark tcl
+
+    ftp -o tcllib.tar.gz "$tcllib_url"
+    tar xzf tcllib.tar.gz
+
+    cd "$tcllib_dir"
+    ./configure
+    make install
 fi
 
 if [ "$(uname)" = OpenBSD ]; then
-    pkg_add -I cmark sqlite3-tcl tcl%8.6 tcllib
+    pkg_add -I cmark sqlite3-tcl tcl%8.6
     ln -s /usr/local/bin/tclsh8.6 /usr/local/bin/tclsh
+
+    ftp -o tcllib.tar.gz "$tcllib_url"
+    tar xzf tcllib.tar.gz
+
+    cd "$tcllib_dir"
+    ./configure
+    make install
 fi
